8a6cd6117de2e7de1b3c3d4142eaa8c89e979cd0
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-04-14 08:38:10 -0300

    Executando comandos em um container Docker

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -7,20 +7,12 @@
 set -e
 
 # shell script que espera o postgres ficar diponível antes do continuar, para não dar erros
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT)..."
-    sleep 2
-done
 
-echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT)"
-
-echo "✔️ Permissões corrigidas, iniciando comandos Django..."
-
+wait_psql.sh
 # coleta os arquivos estaticos
-python manage.py collectstatic --noinput
+collectstatic.sh
 # criando as migrações, antes de migar de fato
-python manage.py makemigrations --noinput
 # Aplica as migrações no banco de dados
-python manage.py migrate --noinput
+migrate.sh
 # roda o server
-python manage.py runserver 0.0.0.0:8000
+runserver.sh
---
+++ scripts/wait_psql.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT)..."
+    sleep 2
+done
+
+echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT)"
+
+echo "✔️ Permissões corrigidas, iniciando comandos Django..."
