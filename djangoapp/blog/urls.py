eb036580395c273cab6252a0d488374bcb1e6ffa
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-04-23 09:46:10 -0300

    Criando os templates necessários para o layout

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
 
 # colocando nome no app para puxar as view mais fácil
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
