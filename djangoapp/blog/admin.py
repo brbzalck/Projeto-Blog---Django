f665dd278fb29d88fb703f550f2265c819f18ef5
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-04-25 09:13:27 -0300

    Criando o model e admin Page (models.Model e admin.ModelAdmin do Django)

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from blog.models import Tag, Category, Page
 
 # Register your models here.
+# Registrando no admin o campo Tag
+@admin.register(Tag)
+# Configurando como será exibido na admin do site
+class TagAdmin(admin.ModelAdmin):
+    # configurando como será exibido a tag
+    list_display = 'id', 'name', 'slug',
+    # em qual campo poderemos acessar ela
+    list_display_links = 'name',
+    # podemos pesquisar por tais campos
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    # colocando uma pré visualização de como a tag vai ficar
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
+# Registrando Page no admin
+@admin.register(Page)
+# com as seguintes informações de exibição
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from utils.rands import slugify_new
 
 # Create your models here.
+# Criando a table Tag que guarda as tags do site
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
+    # Suas colunas serão name e slug
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, null= True, blank=True , max_length=255
+    )
+
+    # ao salvar, executamos nosso método slugify_new
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Pega name que o usuário passou, transforma em slug + 6 random letras
+            self.slug = slugify_new(self.name, 6)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+# table category para as Categorias
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    # Suas colunas serão name e slug
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, null= True, blank=True , max_length=255
+    )
+
+    # ao salvar, executamos nosso método slugify_new
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Pega name que o usuário passou, transforma em slug + 6 random letras
+            self.slug = slugify_new(self.name, 6)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+        
+
+# table para Page
+class Page(models.Model):
+    # titulo da página
+    title = models.CharField(max_length=65,)
+    # slug para salvar no BD
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    # ao salvar, executamos nosso método slugify_new
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Pega name que o usuário passou, transforma em slug + 6 random letras
+            self.slug = slugify_new(self.title, 6)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
